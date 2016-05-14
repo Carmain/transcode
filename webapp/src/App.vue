16ec53dd16468b03136ae5cde88bdcaadfcffb2e
Billard Martin <martin.l.billard@gmail.com>
2016-05-14 14:41:14 +0200

    Adding a navbar

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <hello></hello>
+    <navbar></navbar>
+    <div class ="container">
+      <hello></hello>
+    </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 import Hello from './components/Hello'
 
 export default {
   components: {
+    Navbar,
     Hello
   }
 }
---
+++ webapp/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar navbar-default">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Home</a>
+    </div>
+    <div class="navbar-collapse collapse navbar-right navbar-responsive-collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Connect</a></li>
+        <li><a href="#">Register</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
