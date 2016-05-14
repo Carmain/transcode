47502e1e2d15589431c5266b78dfbd00ae1909fc
Billard Martin <martin.l.billard@gmail.com>
2016-05-14 18:43:09 +0200

    Install vue-router

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+
+import Home from './Home'
+
+Vue.use(VueRouter)
+
+var App = Vue.extend({})
+var router = new VueRouter()
+
+router.map({
+  '/': {
+    component: Home
+  }
+})
+
+router.start(App, '#app')
 
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+// new Vue({
+//   el: 'body',
+//   components: { Home }
+// })
