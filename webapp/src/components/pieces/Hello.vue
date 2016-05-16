1eb1ce216d870e3f043d1ab3d17e673c3fdbd2b6
Billard Martin <martin.l.billard@gmail.com>
2016-05-16 21:07:58 +0200

    Improve the homepage

--- webapp/src/components/pieces/Hello.vue
+++ webapp/src/components/pieces/Hello.vue
@@ -6,25 +6,10 @@
         <div class="row">
           <div class="col-md-4 col-md-offset-4 col-sm-10 col-sm-offset-1">
             <div class="carousel-inner pitch">
-              <div class="item active">
+              <div v-for="quote in quotes" v-bind:class="quote.css_class">
                 <div class="carousel-page">
-                    <p>Transcode always be faster than me !</p>
-                    <small><cite>-- Flash</cite></small>
-                </div>
-              </div>
-
-              <div class="item">
-                <div class="carousel-page">
-                  <p>Please stop using my name in your quotes !</p>
-                  <small><cite>-- Barack Obama</cite></small>
-                </div>
-              </div>
-
-              <div class="item">
-                <div class="carousel-page">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <small><cite>-- Your favorite web designer</cite></small>
-                  <p></p>
+                    <p>{{ quote.message}}</p>
+                    <small><cite>-- {{quote.author}}</cite></small>
                 </div>
               </div>
             </div>
@@ -43,33 +28,13 @@
 
   <div class="square-container text-center">
     <div class="row">
-      <div class="col-md-3 col-sm-6 square">
-        <span class="glyphicon glyphicon-time icon" aria-hidden="true"></span>
+
+      <div v-for="square in squares" class="col-md-3 col-sm-6 square">
+        <span v-bind:class="square.glyphicon" aria-hidden="true"></span>
         <div class="caption">
-            <h2>Lightning Fast!</h2>
+            <h2>{{ square.title }}</h2>
         </div>
-        <p>Your files are converted as fast as possible.</p>
-      </div>
-      <div class="col-md-3 col-sm-6 square">
-        <span class="glyphicon glyphicon-folder-open icon" aria-hidden="true"></span>
-        <div class="caption">
-            <h2>Never lose anymore your files!</h2>
-        </div>
-        <p>We give you 10Gb to store your files for ever.</p>
-      </div>
-      <div class="col-md-3 col-sm-6 square">
-        <span class="glyphicon glyphicon-envelope icon" aria-hidden="true"></span>
-        <div class="caption">
-            <h2>Don't lose your time!</h2>
-        </div>
-        <p>Not the time to wait? You will be notified once the conversion is ready.</p>
-      </div>
-      <div class="col-md-3 col-sm-6 square">
-        <span class="glyphicon glyphicon-user icon" aria-hidden="true"></span>
-        <div class="caption">
-            <h2>Trust is the key!</h2>
-        </div>
-        <p>{{ user_registered }} people trust us. And you ?</p>
+        <p> {{ square.text }}</p>
       </div>
     </div>
   </div>
@@ -80,7 +45,46 @@ export default {
   data () {
     return {
       converted_files: 168276,
-      user_registered: 16616
+      user_registered: 16616,
+      quotes: [
+        {
+          message: 'Transcode always be faster than me !',
+          author: 'Flash',
+          css_class: 'item active'
+        },
+        {
+          message: 'Please stop using my name in your quotes !',
+          author: 'Barack Obama',
+          css_class: 'item'
+        },
+        {
+          message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          author: 'Your favorite web designer',
+          css_class: 'item'
+        }
+      ],
+      squares: [
+        {
+          title: 'Lightning Fast!',
+          text: 'Your files are converted as fast as possible.',
+          glyphicon: 'glyphicon glyphicon-time icon'
+        },
+        {
+          title: 'Never lose anymore your files!',
+          text: 'We give you 10Gb to store your files for ever.',
+          glyphicon: 'glyphicon glyphicon-folder-open icon'
+        },
+        {
+          title: 'Don\'t lose your time!',
+          text: 'Not the time to wait? You will be notified once the conversion is ready.',
+          glyphicon: 'glyphicon glyphicon-envelope icon'
+        },
+        {
+          title: 'Trust is the key!',
+          text: '{{ user_registered }} people trust us. And you ?',
+          glyphicon: 'glyphicon glyphicon-user icon'
+        }
+      ]
     }
   }
 }
