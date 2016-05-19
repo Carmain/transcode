db9d3ce658617724bd4f1661d4fda486854f8a78
MrHalfman <jdutriaux@gmail.com>
2016-05-19 16:14:21 +0200

    Updated auth url

--- api/transcode/transcode/settings.py
+++ api/transcode/transcode/settings.py
@@ -78,6 +78,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': {
+        'rest_framework.permissions.IsAuthenticated',
+    },
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
--- api/transcode/transcode/urls.py
+++ api/transcode/transcode/urls.py
@@ -1,10 +1,10 @@
+from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls import url, include
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 
-
-
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^auth/', obtain_jwt_token),
 ]
