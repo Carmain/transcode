0fea3171afaa35be7f7838a9825930f26bf466c8
Billard Martin <martin.l.billard@gmail.com>
2016-05-20 13:27:20 +0200

    Adding gravatar support (fixes #25)

--- webapp/src/components/Register.vue
+++ webapp/src/components/Register.vue
@@ -3,34 +3,78 @@
     <div class ="container">
       <h1>Registration</h1>
       <form role="form">
-        <div class="form-group">
-          <label class="control-label" for="first-name">First name</label>
-          <input class="form-control" id="first-name" type="text">
+        <div class="row">
+          <div class="col-md-10 col-sm-10">
+            <div class="form-group">
+              <label class="control-label" for="username">Username</label>
+              <input class="form-control" id="username" type="text" v-model="credentials.username">
+            </div>
+            <div class="form-group">
+              <label class="control-label" for="email">Email address</label>
+              <input class="form-control" id="email" type="email" v-model="credentials.email" @change="getGravatar()">
+            </div>
+          </div>
+          <div class="col-md-2 col-sm-2">
+            <img v-bind:src="gravatar"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="form-group col-md-6 col-sm-6">
+            <label class="control-label" for="first-name">First name</label>
+            <input class="form-control" id="first-name" type="text" v-model="credentials.first_name">
+          </div>
+          <div class="form-group col-md-6 col-sm-6">
+            <label class="control-label" for="last-name">Last name</label>
+            <input class="form-control" id="last-name" type="text" v-model="credentials.last_name">
+          </div>
         </div>
         <div class="form-group">
-          <label class="control-label" for="last-name">Last name</label>
-          <input class="form-control" id="last-name" type="text">
-        </div>
-        <div class="form-group">
-          <label class="control-label" for="email">Email address</label>
-          <input class="form-control" id="email" type="email">
+          <label class="control-label" for="birthdate">Birthdate</label>
+          <input class="form-control" id="birthdate" type="date" v-model="credentials.birthdate">
         </div>
         <hr />
         <div class="form-group">
           <label class="control-label" for="password">Password</label>
-          <input class="form-control" id="password" type="password">
+          <input class="form-control" id="password" type="password" v-model="credentials.password">
         </div>
         <div class="form-group">
           <label class="control-label" for="password-confirm">Confirm password</label>
-          <input class="form-control" id="password-confirm" type="password">
-        </div>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox"> Remember me
-          </label>
+          <input class="form-control" id="password-confirm" type="password" v-model="credentials.password_confirmation">
         </div>
         <button type="submit" class="btn btn-raised btn-primary pull-right">Submit</button>
       </form>
     </div>
   </div>
 </template>
+
+<script>
+import md5 from 'blueimp-md5'
+
+  export default {
+    data () {
+      return {
+        gravatar: 'http://www.gravatar.com/avatar/?s=150',
+        credentials: {
+          username: '',
+          email: '',
+          first_name: '',
+          last_name: '',
+          birthdate: '',
+          password: '',
+          password_confirmation: ''
+        }
+      }
+    },
+    ready () {
+    },
+    methods: {
+      getGravatar () {
+        let email = this.credentials.email;
+        if (email != '') {
+          let hash = md5(email);
+          this.gravatar = 'http://www.gravatar.com/avatar/' + hash + "?s=150";
+        }
+      }
+    }
+  };
+</script>
