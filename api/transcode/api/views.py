0ac507d6a39a11e309e60f2add28dfaef52fb346
Billard Martin <martin.l.billard@gmail.com>
2016-05-26 17:04:29 +0200

    Indent the code

--- api/transcode/api/views.py
+++ api/transcode/api/views.py
@@ -104,53 +104,53 @@ class Register(APIView):
 
 
 class BinaryParser(BaseParser):
-  media_type = 'application/octet-stream'
+    media_type = 'application/octet-stream'
 
-  def parse(self, stream, media_type=None, parser_context=None):
-    return stream.read()
+    def parse(self, stream, media_type=None, parser_context=None):
+        return stream.read()
 
 
 class CurrentUserView(APIView):
-  def get(self, request):
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
 
 
 class UploadStart(APIView):
-  parser_classes = (JSONParser,)
+    parser_classes = (JSONParser,)
 
-  def get(self, request):
-    fileUUID = uuid.uuid4().hex
-    filePath = path.join(settings.UPLOAD_DIRECTORY, fileUUID)
+    def get(self, request):
+        fileUUID = uuid.uuid4().hex
+        filePath = path.join(settings.UPLOAD_DIRECTORY, fileUUID)
 
-    return Response({
-      'uuid': fileUUID
-    })
+        return Response({
+            'uuid': fileUUID
+        })
 
 
 class UploadChunk(APIView):
-  parser_classes = (BinaryParser,)
+    parser_classes = (BinaryParser,)
 
-  def post(self, request, uuid):
-    fpath = path.join(settings.UPLOAD_DIRECTORY, uuid)
-    userFile = open(fpath, "ab")
-    userFile.write(bytes(request.data))
+    def post(self, request, uuid):
+        fpath = path.join(settings.UPLOAD_DIRECTORY, uuid)
+        userFile = open(fpath, "ab")
+        userFile.write(bytes(request.data))
 
-    return Response({'success': True})
+        return Response({'success': True})
 
 
 class UploadEnd(APIView):
-  parser_classes = (JSONParser, )
+    parser_classes = (JSONParser, )
 
-  def post(self, request, uuid):
-    user_file_md5 = request.data.get("md5")
-    hash_md5 = hashlib.md5()
-    fpath = path.join(settings.UPLOAD_DIRECTORY, uuid)
-    with open(fpath, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
+    def post(self, request, uuid):
+        user_file_md5 = request.data.get("md5")
+        hash_md5 = hashlib.md5()
+        fpath = path.join(settings.UPLOAD_DIRECTORY, uuid)
+        with open(fpath, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
 
-    hash_md5 = hash_md5.hexdigest()
-    success = user_file_md5 == hash_md5
+        hash_md5 = hash_md5.hexdigest()
+        success = user_file_md5 == hash_md5
 
-    return Response({'success': success})
+        return Response({'success': success})
