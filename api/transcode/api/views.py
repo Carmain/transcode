2c19db34eb975bc66cfd02eaf21b872339feb672
Billard Martin <martin.l.billard@gmail.com>
2016-05-26 16:23:45 +0200

    Error handling on the registration fields

--- api/transcode/api/views.py
+++ api/transcode/api/views.py
@@ -1,3 +1,8 @@
+import hashlib
+import uuid
+import datetime
+import re
+
 from api.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import permission_classes
@@ -8,9 +13,6 @@ from rest_framework.permissions import AllowAny
 from api.serializers import UserSerializer
 from rest_framework import permissions
 from django.conf import settings
-import hashlib
-import uuid
-import datetime
 from os import path
 
 
@@ -22,6 +24,67 @@ class Register(APIView):
         username = request.data.get('username')
         first_name = request.data.get('first_name')
         last_name = request.data.get('last_name')
+        password = request.data.get('password')
+        password_confirmation = request.data.get('password_confirmation')
+
+        if not first_name:
+            return Response({
+                'success': False,
+                'error_msg': 'Missing first name'
+            })
+
+        if not last_name:
+            return Response({
+                'success': False,
+                'error_msg': 'Missing last name'
+            })
+
+        if not username:
+            return Response({
+                'success': False,
+                'error_msg': 'Missing username'
+            })
+        else:
+            if User.objects.filter(username=username).exists():
+                return Response({
+                    'success': False,
+                    'error_msg': 'This username is already used'
+                })
+            else:
+                reg_username = re.compile("[a-zA-Z0-9_-]{3,16}")
+                if not reg_username.match(username):
+                    return Response({
+                        'success': False,
+                        'error_msg': 'The username is not a valid'
+                    })
+
+        if not email:
+            return Response({
+                'success': False,
+                'error_msg': 'Missing email'
+            })
+        else:
+            if User.objects.filter(email=email).exists():
+                return Response({
+                    'success': False,
+                    'error_msg': 'This email is already used'
+                })
+
+        if not password:
+            return Response({
+                'success': False,
+                'error_msg': 'Missing password'
+            })
+        elif not password_confirmation:
+            return Response({
+                'success': False,
+                'error_msg': 'Missing password confirmation'
+            })
+        elif password != password_confirmation:
+            return Response({
+                'success': False,
+                'error_msg': "Password and password confirmation aren't equals"
+            })
 
         user = User.objects.create(
             email=request.data.get('email'),
