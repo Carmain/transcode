844599ad6d9543ae454488c03cf8caeb1cb9a2e7
Billard Martin <martin.l.billard@gmail.com>
2016-06-01 17:04:15 +0200

    Finish the 404 page

--- webapp/src/components/NotFound.vue
+++ webapp/src/components/NotFound.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class ="container">
-    <h1>404 !</h1>
-  </div>
+  <div class="forofor">
+    <div class ="container">
+      <div class="text-center white">
+        <h1 class="title-error">404</h1>
+        <h2>Oh, I’m sorry. Did I break your concentration?</h2>
+      </div>
+    </div>
+    </div>
 </template>
+
+<style scoped>
+
+  .white {
+    color: white;
+  }
+
+  .title-error {
+    font-size: 300px;
+    font-weight: bold;
+    margin-top: 150px;
+  }
+
+  .forofor {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background-image: url("../assets/lost_travolta.gif");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
