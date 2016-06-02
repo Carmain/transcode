b7f9a7afff156cca279d247481fed656b3602019
Billard Martin <martin.l.billard@gmail.com>
2016-06-02 19:51:47 +0200

    Paid with paypal

--- webapp/src/components/Paid.vue
+++ webapp/src/components/Paid.vue
@@ -1,8 +1,47 @@
 <template>
   <div class="container">
-    <h1>Welcome to the $money page BIATCH</h1>
+    <h1>Proceed to paiement</h1>
+    <div id="paypal-container"></div>
   </div>
 </template>
+
+<script>
+import braintree from 'braintree-web';
+import config from "../config.js";
+
+export default {
+  data () {
+    return {
+
+    };
+  },
+  ready () {
+    let that = this;
+    this.$http.get(config.PAYPAL_TOKEN).then((res) => {
+      let paypalToken = res.data.token;
+
+
+      braintree.setup(paypalToken, "custom", {
+        paypal: {
+          container: "paypal-container",
+          singleUse: true,
+          amount: 1.00,
+          currency: 'USD',
+          locale: 'en_us'
+        },
+        onPaymentMethodReceived: (obj) => {
+          that.$http.post(config.PAYPAL_CHECKOUT,
+            {
+              "payment_method_nonce": obj.nonce
+            }).then((res) => {
+            console.log(res);
+          });
+        }
+      });
+    });
+  },
+};
+</script>
 
 <style scoped>
 
