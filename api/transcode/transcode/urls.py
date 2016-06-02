559d82fe626fae9fdcb33c1a1b97ab54e2177c6d
Billard Martin <martin.l.billard@gmail.com>
2016-06-02 18:23:21 +0200

    Reorganize the backend urls

--- api/transcode/transcode/urls.py
+++ api/transcode/transcode/urls.py
@@ -1,22 +1,7 @@
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-from rest_framework import routers
+from api import urls
 from django.conf.urls import url, include
-from api import views as api_views
 
-router = routers.DefaultRouter()
 
 urlpatterns = [
-    url(r'', include(router.urls)),
-    url(r'^api-auth/', include("rest_framework.urls", namespace="rest_framework")),
-    url(r'^auth/$', obtain_jwt_token),
-    url(r'^refresh-jwt/$', refresh_jwt_token),
-    url(r'^me/$', api_views.CurrentUserView.as_view()),
-    url(r'^check-pending/$', api_views.CurrentUserView.as_view()),
-    url(r'^register/$', api_views.Register.as_view()),
-    url(r'^upload-start/$', api_views.UploadStart.as_view()),
-    url(r'^upload-chunk/(?P<uuid>[0-9a-f]{32})/$', api_views.UploadChunk.as_view()),
-    url(r'^upload-end/(?P<uuid>[0-9a-f]{32})/$', api_views.UploadEnd.as_view()),
-    url(r'^paypal-token/$', api_views.getPaypalToken.as_view()),
-    url(r'^checkout/$', api_views.checkout.as_view()),
-    url(r'^launch-conversion/$', api_views.launch_conversion.as_view()),
+    url(r'api/', include(urls.urlpatterns)),
 ]
