f4af9d22e332a36ed0d5fa62205dccd633e9768d
Carmain <martin.l.billard@gmail.com>
2016-06-06 10:52:35 -0700

    PEP8 EXTERMINATOR

--- api/transcode/api/models.py
+++ api/transcode/api/models.py
@@ -9,17 +9,19 @@ TYPES_TABLE = {
     "audio/mpeg": "mp3"
 }
 
+
 def generate_uuid4():
     return uuid.uuid4().hex
+
 
 class FileMixin(object):
     @property
     def fileName(self):
-      return "{0}.{1}".format(self.uuid, self.fileType)
+        return "{0}.{1}".format(self.uuid, self.fileType)
 
     @property
     def path(self):
-      return os.path.join(settings.UPLOAD_DIRECTORY, self.fileName)
+        return os.path.join(settings.UPLOAD_DIRECTORY, self.fileName)
 
 
 class User(AbstractUser):
@@ -30,17 +32,18 @@ class User(AbstractUser):
 
     @property
     def used_space(self):
-      transcode_files = self.transcodefile_set.all()
-      used_space = 0
+        transcode_files = self.transcodefile_set.all()
+        used_space = 0
 
-      for file in transcode_files:
-        used_space += file.size
+        for file in transcode_files:
+            used_space += file.size
 
-      return used_space
+        return used_space
 
     @property
     def free_space(self):
-      return self.disk_space - self.used_space
+        return self.disk_space - self.used_space
+
 
 class ConvertedFile(FileMixin, models.Model):
     fileType = models.CharField(choices=settings.SUPPORTED_FILES, max_length=5)
@@ -54,18 +57,18 @@ class TranscodeFile(FileMixin, models.Model):
     size = models.IntegerField()
 
     def guessFileType(self):
-      print(self.path)
-      mimeType = magic.from_file(self.path, mime=True).decode("utf-8")
-      if mimeType in TYPES_TABLE:
-          return TYPES_TABLE.get(mimeType)
-      return mimeType.split("/")[1]
+        print(self.path)
+        mimeType = magic.from_file(self.path, mime=True).decode("utf-8")
+        if mimeType in TYPES_TABLE:
+            return TYPES_TABLE.get(mimeType)
+        return mimeType.split("/")[1]
 
     def reloadFileType(self):
-      oldPath = self.path
-      self.fileType = self.guessFileType()
-      self.save()
+        oldPath = self.path
+        self.fileType = self.guessFileType()
+        self.save()
 
-      os.rename(oldPath, self.path)
+        os.rename(oldPath, self.path)
 
 
 class UploadSession(models.Model):
@@ -77,11 +80,11 @@ class UploadSession(models.Model):
 
     @property
     def size(self):
-      return self.file.size
+        return self.file.size
 
     @property
     def remainingBytes(self):
-      return self.size - self.receivedBytes
+        return self.size - self.receivedBytes
 
 
 class ConvertJob(models.Model):
