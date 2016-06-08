4f9ffe3e7eca7abc6edc536562d8df3d1d6dc32e
Carmain <martin.l.billard@gmail.com>
2016-06-08 10:17:23 -0700

    Design the success page

--- webapp/src/components/SuccessTranscode.vue
+++ webapp/src/components/SuccessTranscode.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class ="container">
-    <h1>Success !</h1>
+  <div class="forofor">
+    <div class ="container">
+      <div class="text-center white">
+        <h1 class="title-error">Success !</h1>
+        <h2>The uploaded file is being processed. <b>We will send you a message when the conversion is complete.</b></h2>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,3 +31,27 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+  .white {
+    color: white;
+  }
+
+  .title-error {
+    font-size: 200px;
+    font-weight: bold;
+    margin-top: 150px;
+  }
+
+  .forofor {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background-image: url("../assets/dance_drake.gif");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
