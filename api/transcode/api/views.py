a12623c9c2eee557eabdc30da63f414cd3370087
Julien Dutriaux <jdutriaux@gmail.com>
2016-06-09 17:11:26 +0200

    Added audioOnly option (Fixes #18)

--- api/transcode/api/views.py
+++ api/transcode/api/views.py
@@ -232,8 +232,16 @@ class launch_conversion(APIView):
         file_to_convert = TranscodeFile.objects.get(uuid=request.data.get("file"))
         dest_format = request.data.get("format")
         dest_codec = request.data.get("codec")
+        audio_only = request.data.get("audioOnly")
+        if audio_only:
+          dest_format = "mp3"
         dest_type = (dest_format, dest_codec)
-        convert.delay(filePath=file_to_convert.path, fileUUID=file_to_convert.uuid, dest_type=dest_type)
+        convert.delay(
+          filePath=file_to_convert.path,
+          fileUUID=file_to_convert.uuid,
+          dest_type=dest_type,
+          audio_only=audio_only
+        )
         return Response({'success': True})
 
 
--- api/transcode/worker/main.py
+++ api/transcode/worker/main.py
@@ -8,26 +8,36 @@ app.conf.CELERY_SEND_TASK_SENT_EVENT = True
 
 
 @app.task(name="convert")
-def convert(filePath, fileUUID, dest_type=('mkv', 'h264')):
+def convert(filePath, fileUUID, dest_type=('mkv', 'h264'), audio_only=False):
   c = Converter()
   infos = c.probe(filePath)
-  convert_options = {
-    'format': dest_type[0],
-    'audio': {
-      'codec': infos.audio.codec,
-      'samplerate': infos.audio.audio_samplerate,
-      'channels': infos.audio.audio_channels
-    },
-    'video': {
-      'codec': dest_type[1],
-      'width': infos.video.video_width,
-      'height': infos.video.video_height,
-      'fps': infos.video.video_fps
-    },
-    'subtitle': {
-      'codec': 'copy'
+  if audio_only:
+    convert_options = {
+      'format': 'mp3',
+      'audio': {
+        'codec': infos.audio.codec,
+        'samplerate': infos.audio.audio_samplerate,
+        'channels': infos.audio.audio_channels
+      }
     }
-  }
+  else:
+    convert_options = {
+      'format': dest_type[0],
+      'audio': {
+        'codec': infos.audio.codec,
+        'samplerate': infos.audio.audio_samplerate,
+        'channels': infos.audio.audio_channels
+      },
+      'video': {
+        'codec': dest_type[1],
+        'width': infos.video.video_width,
+        'height': infos.video.video_height,
+        'fps': infos.video.video_fps
+      },
+      'subtitle': {
+        'codec': 'copy'
+      }
+    }
   dirPath = path.dirname(filePath)
   baseName = path.splitext(path.basename(filePath))[0]
   newFileName = "{}.{}".format(baseName, convert_options.get("format"))
