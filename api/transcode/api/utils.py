9d0c79da01070ea9b52bd53e553168792cd7e3ca
Julien Dutriaux <jdutriaux@gmail.com>
2016-06-09 15:14:35 +0200

    Updated email (Fixes #40)

--- api/transcode/api/utils.py
+++ api/transcode/api/utils.py
@@ -6,10 +6,23 @@ def get_price(duration):
   return max(price, 0.01)
 
 def send_completion_mail(user, transcode_file):
+  textual_mail_body = "You can view your transcoded files on the homepage when you are connected."
+  html_mail_body = """
+    <html>
+      <body>
+        <h1>Your file is ready!</h1>
+        <p>
+          Download your transcoded file <a href='http://{}/'>here</a>! (Don't forget to log in)
+        </p>
+      </body>
+    </html>
+  """.format(settings.BASE_DOMAIN)
+
   send_mail(
     "Your file has been successfully transcoded.",
-    transcode_file.uuid,
+    textual_mail_body,
     "Transcode <transcode@gmail.com>",
     [user.email],
-    fail_silently=True
+    fail_silently=True,
+    html_mail=html_mail_body
   )
