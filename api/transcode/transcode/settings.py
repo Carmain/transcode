589de2642e8fea37adbf0ed6708660ec92f9ee4d
Julien Dutriaux <jdutriaux@gmail.com>
2016-06-10 15:21:34 +0200

    Added json default renderer for djrf

--- api/transcode/transcode/settings.py
+++ api/transcode/transcode/settings.py
@@ -74,6 +74,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 JWT_AUTH = {
