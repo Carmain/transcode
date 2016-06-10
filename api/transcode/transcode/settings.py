c9358581b4b7906bd9cc42d75494a8f0929ec3f1
Julien Dutriaux <jdutriaux@gmail.com>
2016-06-10 15:06:49 +0200

    Fixed errors in settings.

--- api/transcode/transcode/settings.py
+++ api/transcode/transcode/settings.py
@@ -29,12 +29,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_extensions',
     'api',
     'rest_framework'
